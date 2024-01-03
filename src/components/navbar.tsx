104eba15d18f72da79a45685bf1712c45affef10
Olimov Abdulloh <lochin.tj@gmail.com>
2024-01-03 14:43:59 +0500

    add navbar in shop

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,10 +9,23 @@ const Navbar = () => {
         <Image src={"/logo.svg"} alt="logo" width={150} height={140} />
         <h1 className="text-red-400">hhh</h1>
       </Link>
-      <div className="flex items-center space-x-2.5 text-sm ">
-        <button className="button bg-blue-600 text-white border border-transparent  hover:border-blue-600 hover:bg-transparent hover:text-black">
-          Log in
-        </button>
+      <div className="flex items-center">
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <Link href={"/"} className="mr-5 hover:text-gray-900">
+            Home page
+          </Link>
+          <Link href={"/"} className="mr-5 hover:text-gray-900">
+            All productsk
+          </Link>
+        </nav>
+        <div className="flex items-center space-x-2.5 text-sm ">
+          <button className="button bg-blue-600 text-white border border-transparent  hover:border-blue-600 hover:bg-transparent hover:text-black">
+            Log in
+          </button>
+          <button className="button bg-transparent   border-blue-600  hover:border-transparent  hover:bg-blue-600  hover:text-white ">
+            Sign up
+          </button>
+        </div>
       </div>
     </div>
   );
