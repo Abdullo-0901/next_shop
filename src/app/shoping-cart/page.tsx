601fbb709d9a31f02dbeea0cbeef12c02f5d8ace
Olimov Abdulloh <lochin.tj@gmail.com>
2024-01-04 16:23:29 +0500

    show price product

--- src/app/shoping-cart/page.tsx
+++ src/app/shoping-cart/page.tsx
@@ -74,7 +74,7 @@ const ShopingCart = () => {
                       <input
                         className="h-8 w-8 border bg-white text-center text-xs outline-none"
                         type="number"
-                        value="2"
+                        value={c.quantity}
                         min="1"
                       />
                       <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
@@ -83,7 +83,7 @@ const ShopingCart = () => {
                       </span>
                     </div>
                     <div className="flex items-center space-x-4">
-                      <p className="text-sm">259.000 ₭</p>
+                      <p className="text-sm">${c.price}</p>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
